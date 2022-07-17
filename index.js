83ffc5b9efc641b15c8c205225917c5bd29c6ed1
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-17 16:05:18 -0400

    changed htmlstring to readme.md, updated choices for liscense

--- index.js
+++ index.js
@@ -36,9 +36,7 @@ inquirer.prompt([
         name: "license",
         message:"Which is your preferred liscense?",
         type:"checkbox",
-        choices: ["MIT License Copyright (c) 2022 Annie Hosch Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.",
-                    "other",
-                    "another other"]
+        choices:["MIT", "other", "another other"]
     },
     {
         name: "questions",
@@ -52,53 +50,36 @@ inquirer.prompt([
     }
 ]).then(answers => {
     console.log(answers)
-    const htmlString = `
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        
-                        <head>
-                        <meta charset="UTF-8">
-                        <meta http-equiv="X-UA-Compatible" content = "IE=edge">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>README</title>>
-                        </head>
+    const README = `
+# ${answers.title}
+## Description
+${answers.description}
 
-                        <body>
-                        <h1>${answers.title}</h1>
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
 
-                            <h2>Description</h2>
-                            <p>${answers.description}</p>
+## Installation
+${answers.installation}
 
-                            <h2>Table of Contents</h2>
-                            <p>
-                            - [Installation](#installation)
-                            - [Usage](#usage)
-                            - [Credits](#credits)
-                            - [License](#license)
-                            </p>
-                            
-                            <h2>Installation</h2>
-                            <p>${answers.installation}</p>
+## Usage
+${answers.usage}
 
-                            <h2>Usage</h2>
-                            <p>${answers.usage}</p>
-
-                            <h2>Liscense</h2>
-                            <p>${answers.liscense}</p>
+## Liscense
+MIT License Copyright (c) 2022 Annie Hosch Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                            
-                            <h2>Contributing</h2
-                            <p>${answers.contribute}</p>
+## Contributing
+${answers.contribute}
 
-                            <h2>Tests</h2>
-                            <p>${answers.test}</p>
+## Tests
+${answers.test}
 
-                            <h2>Questions</h2>
-                            <pre>Have any questions?
-                                github.com/${answers.questions}  
-                                Additional questions can be sent to the following address: ${answers.questions2}
-                            </pre>
-                        </body>
-                        </html>
+## Questions
+Have any questions?
+github.com/${answers.questions}  
+Additional questions can be sent to the following address: ${answers.questions2}                 
     `
-    fs.writeFile(`./output/${answers.title}.md`, htmlString, (err) => err ? console.error(err) : console.log('success!'));
+    fs.writeFile(`./output/${answers.title}.md`, README, (err) => err ? console.error(err) : console.log('success!'));
 })
---
+++ output/yet another testcase.md
@@ -0,0 +1,31 @@
+
+yet another testcase
+Description
+Changed html to md
+
+Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+Installation
+run node
+
+Usage
+add an assests folder
+
+Liscense
+undefined
+                           
+Contributing
+oa
+
+Tests
+Text sample
+
+Questions
+Have any questions?
+github.com/example  
+Additional questions can be sent to the following address: ax@email.com                 
+    
